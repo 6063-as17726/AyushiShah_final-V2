5fd308de2d559395f980c850e8564f4ff975465e
Ayushi Shah <as17726@nyu.edu>
2023-12-05 16:33:10 -0500

    Laid out code components

--- README.md
+++ README.md
@@ -1,21 +1,22 @@
 Final_Week 2:
 I decided to go ahead with the idea of creating an interactive storytelling. But instead of a classic children's story, I chose to represent real, diverse stories about girls beginning their period as a means of awareness for young girls.
 Interactions
-Input: Clicks on the computer, button on Arduino
-Output: Visuals on the computer, LED & sound & Haptics on Arduino
-System diagram
-FSM diagram
-Circuit diagram
+  Input: Clicks on the computer, button on Arduino
+  Output: Visuals on the computer, LED & sound & Haptics on Arduino
 Description of any external data or library that you are planning to use
-Description of any sensor, output component or mechanism that you are planning on using or building
+  I would love to try and use a library to draw the characters. Ex. - https://github.com/gaba5/p5.shape.js
 Reference images, texts and projects
     https://storymaps.arcgis.com/stories/4f8da19484ea46e0abaacee95312f26c
 Plan for user testing
   I will ask different people to follow the instructions and document if there are any instructions they are unclear with
 Short discussion of why your project is relevant:how is it related to our readings? how does it connected to society? why is it important to you?
+  I read somewhere that women were not considered in medical related research till about 1993. Because of this, a lot of healthcare today is not mindful of the actual needs of women. Keeping this in mind, I wanted to create a space to tell stories of different women's journey with their health. 
 Pseudocode or an outline of how you plan to organize your code
+  In the sketch file 
 Documentation of visual experiments
+  ![Alt text](IMG_4474.jpg)
 Description of any code written so far
+  Currently, I have laid out a rough layout of what conponents will make up the code. 
 
 HW12: 
 I chose to do a serial connection between Arduino and p5js. I chose to create a serial connection due to it's simplicity and since my Arduino circuit mainly consisted of only buttons, I thought it wouldn't be too difficult to just keep the Arduino connected the entire time. Moreover, since the Wi-Fi connection does not work in the school campus, I thought that would limit me to only work on it at home.
@@ -26,8 +27,6 @@ FSM and circuit diagram respectively
 ![Alt text](IMG_4473.jpg)
 Image of circuit
 
-../../../Desktop/IMG_4471.MOV
-Video of working 
 
 
 
--- sketch.js
+++ sketch.js
@@ -1,18 +1,48 @@
 let mSerial;
 let readyToRead;
 
-// function character1() {
+let characterWidth = 50; 
+let characterHeight = 150; 
 
+// function character1() {
+//x1, y1
 // }
 // function character2() {
-
+  //x2, y2
 // }
 // function character3() {
-
+  //x3, y3
 // }
 // function character4() {
+  //x4, y4
+// }
+
+// function description1() {
 
 // }
+// function description2() {
+
+// }
+// function desciption3() {
+
+// }
+// function description4() {
+
+// }
+
+// function story1() {
+  //button 1 - click through story, audio, visuals 
+// }
+// function story2() {
+  //button 1 - click through story, audio, visuals 
+// }
+// function story3() {
+  //button 1 - click through story, audio, visuals 
+// }
+// function story4() {
+  //button 1 - click through story, audio, visuals 
+// }
+
 
 function connectToSerial() {
   mSerial.open(9600);
@@ -31,6 +61,42 @@ function setup() {
   connectButton.position(width / 2, height / 2);
   connectButton.mousePressed(connectToSerial);
 }
+
+// function mouseClicked() {
+//   if (
+//     mouseX > x1 &&
+//     mouseX < x1 + characterWidth &&
+//     mouseY > y1 &&
+//     mouseY < y1 + characterHeight
+//     )  
+//      { story1();
+//       }
+//    else if (
+//     mouseX > x2 &&
+//     mouseX < x2 + characterWidth &&
+//     mouseY > y2 &&
+//     mouseY < y2 + characterHeight
+//     )  
+//      { story2();
+//       }
+//    else if (
+//     mouseX > x3 &&
+//     mouseX < x3 + characterWidth &&
+//     mouseY > y3 &&
+//     mouseY < y3 + characterHeight
+//     )  
+//      { story3();
+//       }
+//    else if (
+//     mouseX > x4 &&
+//     mouseX < x4 + characterWidth &&
+//     mouseY > y4 &&
+//     mouseY < y4 + characterHeight
+//     )  
+//      { story4();
+//       }
+
+// }
 
 function draw() {
 
@@ -52,5 +118,42 @@ function draw() {
     //character2(); 
     //character3(); 
     //character4();
+
+    //hover preview
+    // if (
+    //   mouseX > x1 &&
+    //   mouseX < x1 + characterWidth &&
+    //   mouseY > y1 &&
+    //   mouseY < y1 + characterHeight 
+    //   ){
+    //     description1(); 
+    //    }
+     // if (
+    //   mouseX > x2 &&
+    //   mouseX < x2 + characterWidth &&
+    //   mouseY > y2 &&
+    //   mouseY < y2 + characterHeight 
+    //   ){
+    //     description2(); 
+    //    }
+     // if (
+    //   mouseX > x3 &&
+    //   mouseX < x3 + characterWidth &&
+    //   mouseY > y3 &&
+    //   mouseY < y3 + characterHeight 
+    //   ){
+    //     description3(); 
+    //    }
+     // if (
+    //   mouseX > x4 &&
+    //   mouseX < x4 + characterWidth &&
+    //   mouseY > y4 &&
+    //   mouseY < y4 + characterHeight 
+    //   ){
+    //     description4(); 
+    //    }
+  
+  //button 2 - return to homepage 
+
 }
   
